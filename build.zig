49b782ac1ccfb526841e7d9076b510b44c19d7d3
William D. Jones <thor0505@comcast.net>
2020-09-05 18:24:26 -0400

    Add extern directory for extra libraries, update day4 and build.zig accordingly.

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Builder = std.build.Builder;
+const LibExeObjStep  = std.build.LibExeObjStep;
 const fmt = std.fmt;
 
 pub fn build(b: *Builder) void {
@@ -11,10 +12,22 @@ pub fn build(b: *Builder) void {
         const path_name = fmt.bufPrint(&buf, "{}/{}.zig", .{ day, day }) catch unreachable;
         const day_exe = b.addExecutable(day, b.dupe(path_name));
         day_exe.setBuildMode(b.standardReleaseOptions());
+        add_extern(b, day_exe);
 
         const day_cmd = day_exe.run();
         const run_msg = fmt.bufPrint(&buf, "Run AOC 2016 Day {} Program", .{day_no}) catch unreachable;
         const day_step = b.step(day, b.dupe(run_msg));
         day_step.dependOn(&day_cmd.step);
+    }
+}
+
+// Add external libraries used here- there are available as submodules under extern.
+fn add_extern(b: *Builder, e: *LibExeObjStep) void {
+    for ([_][2][]const u8{
+            [2][]const u8{"regex", "extern/zig-regex/src/regex.zig"},
+
+        }) |lib_path| {
+
+        e.addPackagePath(lib_path[0], lib_path[1]);
     }
 }
--- day4/day4.zig
+++ day4/day4.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
+const Regex = @import("regex").Regex;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("AOC 2016 Day 4\n", .{});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var re = try Regex.compile(&gpa.allocator, "\\w+");
+    defer re.deinit();
 }
