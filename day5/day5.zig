c069fa8e04726d23acef9cd1a2bcf922a97a2495
William D. Jones <thor0505@comcast.net>
2020-09-06 21:45:06 -0400

    Complete Day 5, Part 2.

--- day5/day5.zig
+++ day5/day5.zig
@@ -3,6 +3,8 @@ const Md5 = std.crypto.hash.Md5;
 const mem = std.mem;
 const fmt = std.fmt;
 const expectEqual = std.testing.expectEqual;
+const AutoHashMap = std.hash_map.AutoHashMap;
+const Allocator = std.mem.Allocator;
 
 const ImpliedPos = struct {
     pass: [4]u8 = undefined,
@@ -10,8 +12,6 @@ const ImpliedPos = struct {
     ready: bool = false,
 
     fn add_char(self: *ImpliedPos, candidate: [2]u8) !void {
-        const stdout = std.io.getStdOut().writer();
-
         if(candidate[0] >= 16) {
             return error.OutOfRange;
         }
@@ -28,6 +28,63 @@ const ImpliedPos = struct {
     }
 };
 
+const CalcPos = struct {
+    pass: [4]u8 = undefined,
+    seen: AutoHashMap(u8, u8),
+    count: u8 = 0,
+    ready: bool = false,
+
+    fn init(a: *Allocator) CalcPos {
+        var seen = AutoHashMap(u8, u8).init(a);
+
+        return CalcPos {
+            .seen = seen
+        };
+    }
+
+    fn add_char(self: *CalcPos, candidate: [2]u8) !void {
+        var pos = candidate[0];
+        var char = candidate[1];
+
+        if(pos >= 8) {
+            return;
+        }
+
+        if(char >= 16) {
+            return error.OutOfRange;
+        }
+
+        var res = try self.seen.getOrPut(pos);
+        if(res.found_existing) {
+            return;
+        } else {
+            res.entry.value = char;
+            self.count += 1;
+        }
+
+        if(self.count >= 8) {
+            var curr_count: u8 = 0;
+
+            while(curr_count < self.count) : (curr_count += 1) {
+                var curr_char = try self.seen.getOrPut(curr_count);
+
+                if(curr_count % 2 == 1) {
+                    self.pass[curr_count / 2] |= curr_char.entry.value;
+                } else {
+                    self.pass[curr_count / 2] = 0;
+                    self.pass[curr_count / 2] = curr_char.entry.value << 4;
+                }
+            }
+
+            self.ready = true;
+        }
+    }
+
+    fn deinit(self: *CalcPos) void {
+        self.seen.deinit();
+    }
+};
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("AOC 2016 Day 5\n", .{});
@@ -37,6 +94,14 @@ pub fn main() !void {
     var strat = ImpliedPos{};
     try find_password(ImpliedPos, &strat, input);
     try stdout.print("{X}\n", .{strat.pass});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var new_strat = CalcPos.init(&gpa.allocator);
+    defer _ = gpa.deinit();
+    defer new_strat.deinit();
+
+    try find_password(CalcPos, &new_strat, input);
+    try stdout.print("{X}\n", .{new_strat.pass});
 }
 
 fn find_password(comptime T: type, strategy: *T, id: []const u8) !void {
@@ -81,4 +146,11 @@ test "abc" {
     var strat = ImpliedPos{};
     try find_password(ImpliedPos, &strat, "abc");
     try stdout.print("{X}\n", .{strat.pass});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var new_strat = CalcPos.init(&gpa.allocator);
+    defer _ = gpa.deinit();
+    defer new_strat.deinit();
+    try find_password(CalcPos, &new_strat, "abc");
+    try stdout.print("{X}\n", .{new_strat.pass});
 }
