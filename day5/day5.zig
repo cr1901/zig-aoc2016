88299fc23ad07de4624cc68b9b060b632329aef7
William D. Jones <thor0505@comcast.net>
2020-09-06 21:02:14 -0400

    Make Day 5, Part 1 generic to prepare for Part 2.

--- day5/day5.zig
+++ day5/day5.zig
@@ -4,41 +4,55 @@ const mem = std.mem;
 const fmt = std.fmt;
 const expectEqual = std.testing.expectEqual;
 
+const ImpliedPos = struct {
+    pass: [4]u8 = undefined,
+    count: u8 = 0,
+    ready: bool = false,
+
+    fn add_char(self: *ImpliedPos, candidate: [2]u8) !void {
+        const stdout = std.io.getStdOut().writer();
+
+        if(candidate[0] >= 16) {
+            return error.OutOfRange;
+        }
+
+        if(self.count % 2 == 1) {
+            self.pass[self.count / 2] |= candidate[0];
+        } else {
+            self.pass[self.count / 2] = 0;
+            self.pass[self.count / 2] = candidate[0] << 4;
+        }
+
+        self.count += 1;
+        self.ready = (self.count >= 8);
+    }
+};
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("AOC 2016 Day 5\n", .{});
 
-    var pass: [4]u8 = undefined;
     var input = "cxdnnyjw";
 
-    try find_password(input, &pass);
-    try stdout.print("{X}\n", .{pass});
+    var strat = ImpliedPos{};
+    try find_password(ImpliedPos, &strat, input);
+    try stdout.print("{X}\n", .{strat.pass});
 }
 
-fn find_password(id: []const u8, pass: *[4]u8) !void {
+fn find_password(comptime T: type, strategy: *T, id: []const u8) !void {
     var idx : u32 = 0;
-    var num_chars: u8 = 0;
 
-    const stdout = std.io.getStdOut().writer();
+    // ./day5/day5.zig:50:13: error: container 'ImpliedPos' has no member called 'ready'
+    // while(!T.ready) {
+    while(!strategy.ready) : (idx += 1) {
+        if(try find_candidate(id, idx)) |c| {
+            try strategy.add_char(c);
 
-    next: while(num_chars < 8) : (num_chars += 1) {
-        while(true) : (idx += 1) {
-            if(try find_candidate(id, idx)) |c| {
-                if(num_chars % 2 == 1) {
-                    pass[num_chars / 2] |= c;
-                } else {
-                    pass[num_chars / 2] = 0;
-                    pass[num_chars / 2] = c << 4;
-                }
-
-                idx += 1;
-                continue :next;
-            }
         }
     }
 }
 
-fn find_candidate(id: []const u8, num: u32) !?u8 {
+fn find_candidate(id: []const u8, num: u32) !?[2]u8 {
     var buf: [24]u8 = undefined;
     var out: [16]u8 = undefined;
 
@@ -54,7 +68,8 @@ fn find_candidate(id: []const u8, num: u32) !?u8 {
     Md5.hash(buf[0..buf_len], &out, .{});
 
     if(out[0] == 0 and out[1] == 0 and (out[2] >> 4) == 0) {
-        return out[2] & 0x0f;
+        var chars: [2]u8 = [2]u8{out[2] & 0x0f, out[3] >> 4};
+        return chars;
     } else {
         return null;
     }
@@ -63,7 +78,7 @@ fn find_candidate(id: []const u8, num: u32) !?u8 {
 test "abc" {
     const stdout = std.io.getStdOut().writer();
 
-    var pass: [4]u8 = undefined;
-    try find_password("abc", &pass);
-    try stdout.print("{X}\n", .{pass});
+    var strat = ImpliedPos{};
+    try find_password(ImpliedPos, &strat, "abc");
+    try stdout.print("{X}\n", .{strat.pass});
 }
